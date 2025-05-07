654ef8f7c42763f5d3d0c3ae91545eba072ef42a
kyleq1 <kylequinlan09@gmail.com>
2025-05-07 07:01:22 -0700

    Formatting

--- exo_microros/include/amt22.hpp
+++ exo_microros/include/amt22.hpp
@@ -12,12 +12,12 @@ public:
 
   float angleRad() const;
   float velocityRadS() const;
-
+  
 private:
   static constexpr float    INV_LSB   = 2.0f * PI / 16384.0f; // 2π/2¹⁴
 
   bool readRaw(uint16_t &raw);
-
+  
   uint8_t cs_pin_;
   uint16_t last_raw_;
   uint32_t last_us_;
--- exo_microros/include/bno085.hpp
+++ exo_microros/include/bno085.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+
 #include <Adafruit_BNO08x.h>
 #include "shared.hpp"
 
